0211ffb8e69ed87a8a352ab7d2910dc81a7a535d
Wilssoooonnn <adamnuralifi11@gmail.com>
2025-03-13 19:11:35 +0700

    Praktikum 5

---
+++ app/Http/Controllers/KategoriController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class KategoriController extends Controller
+{
+    public function index()
+    {
+        // $data = [
+        //     'kategori_kode' => 'SNK',
+        //     'kategori_nama' => 'Snack/Makanan Ringan',
+        //     'created_at' => NOW()
+        // ];
+
+        // DB::table('m_kategori')->insert($data);
+        // return 'Insert Data Berhasil';
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->update([
+        //     'kategori_nama' => 'Camilan'
+        // ]);
+
+        // return 'Update Data Berhasil. Jumlah data yang diupdate : ' . $row;
+
+        // $row = DB::table('m_kategori')->where('kategori_kode', 'SNK')->delete();
+        // return 'Delete Data Berhasil. Jumlah data yang dihapus : ' . $row;
+
+        $data = DB::table('m_kategori')->get();
+        return view('kategori', ['data' => $data]);
+    }
+}
