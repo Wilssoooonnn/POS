4a9cd4cb6e68d400fe00793e78fd4705af40d9fb
Wilssoooonnn <adamnuralifi11@gmail.com>
2025-03-13 18:31:45 +0700

    PRAKTIKUM 4

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\SalesController;
+use App\Http\Controllers\LevelController;
 
-Route::get('/', [HomeController::class, 'index']);
-
-Route::prefix('category')->group(function () {
-    Route::get('/food-beverage', [ProductController::class, 'foodBeverage']);
-    Route::get('/home-care', [ProductController::class, 'homeCare']);
-    Route::get('/baby-kid', [ProductController::class, 'babyKid']);
+Route::get('/', function () {
+    return view('welcome');
 });
 
-Route::get('/user/{id}/name/{name}', [UserController::class, 'profile']);
-
-Route::get('/category/beauty-health', [SalesController::class, 'index']);
+Route::get('/level', [LevelController::class, 'index']);
